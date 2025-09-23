03555caf352cb73f968ea9547e9c51800234e1a7
vasanth <vasanth@LAPTOP-GI1KVASF>
2025-09-23 15:15:42 +0530

    Committed by Kesavan

---
+++ CORE_JAVA/src/DAY1/First_project.java
@@ -0,0 +1,11 @@
+package DAY1;
+
+public class First_project {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Welcome To Java...");
+
+	}
+
+}
