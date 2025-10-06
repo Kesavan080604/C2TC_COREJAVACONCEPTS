34bc338d9a0ea33f68c8d6b05b3281018baa00b8
vasanth <vasanth@LAPTOP-GI1KVASF>
2025-10-06 11:57:27 +0530

    day4 completed

---
+++ CORE_JAVA/src/com/tnsif/ifet/day4/SecondPackage/Executor.java
@@ -0,0 +1,14 @@
+package com.tnsif.ifet.day4.SecondPackage;
+
+import com.tnsif.ifet.day4FirstPackage.Base;
+
+public class Executor {
+	public static void main(String[] args) {
+
+		Base b1 = new Base();
+		b1.methodPublic();
+		b1.varPublic = 41;
+		b1.methodPublic();
+	}
+
+}
---
+++ CORE_JAVA/src/com/tnsif/ifet/day4/ThirdPackage/PrivateConstructor.java
@@ -0,0 +1,15 @@
+package com.tnsif.ifet.day4.ThirdPackage;
+
+public class PrivateConstructor {
+	public static void main(String s[]) {
+		MyClass m=MyClass.getObject();
+		m.setId(4);
+		
+		MyClass m1=MyClass.getObject();
+		
+		System.out.println(m);
+		System.out.println(m1);
+		
+	}
+
+}
