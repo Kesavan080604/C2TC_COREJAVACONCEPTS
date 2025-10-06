f879b103aa63804779701971c703459f3c22ab0f
vasanth <vasanth@LAPTOP-GI1KVASF>
2025-10-06 11:05:05 +0530

    day3 completed

---
+++ CORE_JAVA/src/com/tnsif/ifet/day3/Encapsulation/Encapsulation.java
@@ -0,0 +1,15 @@
+package com.tnsif.ifet.day3.Encapsulation;
+
+public class Encapsulation {
+public static void main(String[] args) {
+		
+		//object creation 
+		OopsConcept obj = new OopsConcept();
+		obj.setSerialNum(4);
+		obj.setName("KESAVAN");
+		obj.setAge(21);
+		System.out.println(obj);
+		
+	}
+
+}
---
+++ CORE_JAVA/src/com/tnsif/ifet/day3/Example/Main.java
@@ -0,0 +1,15 @@
+
+package com.tnsif.ifet.day3.Example;
+
+public class Main {
+	public static void main(String[] args) {
+
+        Car car1 = new Car("Tesla", 2023);
+        Car car2 = new Car("Toyota", 2020);
+        Car car3 = new Car("Honda", 2018);
+        car1.showInfo();
+        car2.showInfo();
+        car3.showInfo();
+    }
+
+}
