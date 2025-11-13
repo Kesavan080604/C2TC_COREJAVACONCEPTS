66b2969107ca2a83be8d036a1154352cbb6dd7f5
ASUS <ASUS@LAPTOP-JS9VE7SK>
2025-11-13 09:52:28 +0530

    All 24 projects updated

---
+++ CoreJavaPrograms/src/tnsiif/ifet/dayone/FirstProgram.java
@@ -0,0 +1,8 @@
+//Demonstration of simple java program
+package tnsiif.ifet.dayone;
+
+public class FirstProgram {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
---
+++ CoreJavaPrograms/src/tnsiif/ifet/dayone/test.java
@@ -0,0 +1,11 @@
+package tnsiif.ifet.dayone;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("HI");
+
+	}
+
+}
